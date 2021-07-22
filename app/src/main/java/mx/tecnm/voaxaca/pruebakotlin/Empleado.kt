ecff2913e1a3dd4b372f2e0920a166e9d6764093
Kevin David Molina Gomez <kevin.molina.go@gmail.com>
2021-07-22 10:11:54 -0500

    Ejemplo SOLID 1. Viola principio solid.

---
+++ app/src/main/java/mx/tecnm/voaxaca/pruebakotlin/Empleado.kt
@@ -0,0 +1,28 @@
+package mx.tecnm.voaxaca.pruebakotlin
+
+class Empleado {
+
+    // Atributos
+
+    val name = "KEVIN DAVID MOLINA GOMEZ"
+    val age = 24
+
+    // FUNCIONES
+
+    /** Una funcion que pueda cambiarla el de Finanzas **/
+    fun calcularPago(){
+        // Calcular el salario del empleado. ICR, SEGURO...
+    }
+
+    /** Puede cambiarla Recursos Humanos **/
+    fun reportarHoras(){
+        // Calcular horas trabajadas, Bonos, Emepleado
+    }
+
+    /** Puede cambiarlo del jefe de tecnologia **/
+    fun guardar(){
+        // Guardar la informacion en una base de datos , o web service.
+    }
+
+
+}
