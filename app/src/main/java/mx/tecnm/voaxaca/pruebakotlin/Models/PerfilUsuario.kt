4769c8779d306ed03e28b8fc16774a8b59fecc5e
dakedroid <kevin.molina.go@gmail.com>
2021-07-27 19:26:52 -0500

    Ejercicio SOLID 2 terminado ...

--- app/src/main/java/mx/tecnm/voaxaca/pruebakotlin/Models/PerfilUsuario.kt
+++ app/src/main/java/mx/tecnm/voaxaca/pruebakotlin/Models/PerfilUsuario.kt
@@ -3,17 +3,6 @@ package mx.tecnm.voaxaca.pruebakotlin
 import org.json.JSONObject
 
 
-/**
- *      1. Mostrar publicaciones de usuarios - UI/UX - Nuevo tipo (Videos, Gift, VIVO)
- *      2. Reportar - Restricciones o Cumplimiento - Abogados derecho Mercantil
- *      3. Bloquear - Restricciones o Cumplimiento - Abogados derecho Mercantil
- *      4. Enviar Mensaje - Nuevo tipo (Video, Audio, Contactos)
- *      5. Ver Conversacion - UI/UX - Nuevo Tipo (Video, Audio, Contactos )
- *      6. Mostrar lista de amigos - UI/UX
- *      7. Enviar Solicitud - No deberia cambiar ...
- *      8. Quitar amigo - No deberia cambiar ...
- *      9. Desbloquear un amigo - Restricciones o Cumplimiento - Abogados derecho Mercantil
- * **/
 
 class PerfilUsuario {
 
